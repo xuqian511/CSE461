37d5fc3f292e4892a3c972ab19460cc4e8102650
honkuro1227 <honkuro@uw.edu>
2020-08-13 02:23:27 +0800

    project3

--- project3/proxy.py
+++ project3/proxy.py
@@ -1,156 +1,185 @@
+from sys import argv, exit
 import socket
 import threading
-import sys
-import select
-import datetime
+import re
 
-class Client_handler(threading.Thread):
+LOCAL_HOST = "127.0.0.1"
 
-	def __init__(self, proxy_socket, client_conn):
-		threading.Thread.__init__(self)
-		self.proxy_socket = proxy_socket
-		self.client_conn = client_conn
+class BrowserThread(threading.Thread):
+    def __init__(self,browserSock, browserAddr):
+        threading.Thread.__init__(self)
+        self.bSock = browserSock
+        self.bAddr = browserAddr
+        
+    def run(self):
+        currLine = ""
+        request = ""
+        header = ""
+        serverName = ""
+        serverPort = 80
 
-	def run(self):
-		# recevie the tcp payload
-		try:
-			data = client_conn.recv(8192)
+        while True:
+            if header[-4:] == "\r\n\r\n" or header[-3:-1] == "\n\r\n":
+                # If header end with "\r\n\r\n" or "\n\r\n", we've got the whole header
+                break
+            try:
+                currLine += self.bSock.recv(1).decode()
+            except socket.timeout:
+                print("Didn't receive any data from this socket")
+                print(currLine, request, header)
+                self.bSock.close()
+                return
+            except socket.error:
+                # If the socket is good, but we got a non fatal error indicating
+                # that we've received nothing, we should try again.
+                continue
+            if currLine[-1:] == "\n":
+                # if we still haven't proccess the first line
+                if len(request) == 0:
+                    # Change http version to 1.0
+                    lineSplit = re.split('HTTP/', currLine)
+                    currLine = lineSplit[0] + "HTTP/1.0\r\n"
+                    request = currLine
 
-			strings = bytes(data)
+                    # print out request line
+                    print(">>> " + request[:-2])
 
-			http_version_idx = strings.find('HTTP/1.1')
-			connection_idx = strings.find('keep-alive')
-			second_connection_idx = strings.find('keep-alive', connection_idx + 10)
+                    # assign initial server's port number based on the request line
+                    reqSplit = re.split(' |:', request)
+                    if len(reqSplit) == 5:
+                        serverPort = int(reqSplit[3])
+                    elif len(reqSplit) == 4 and reqSplit[0] == "CONNECT":
+                        serverPort = int(reqSplit[2])
+                    elif reqSplit[1].lower() is "https":
+                        serverPort = 443
 
-			header_end_idx = strings.find('\r\n\r\n')
-			forward_request = strings[0:http_version_idx + 7] + '0' + strings[http_version_idx + 8:connection_idx] + 'close' 
+                # if this line has the host keyword, we want to get the domain and port of the server
+                elif "host" in currLine.lower():
+                    lineSplit = re.split(' :|: |:', currLine)
+                    if len(lineSplit) == 3:
+                        # Enters here if we have a port number in the Host line.
+                        serverName = lineSplit[1]
+                        port = int(lineSplit[2][:-2])
+                    else:
+                        serverName = lineSplit[1][:-2]
 
-			if second_connection_idx == -1:
-				forward_request = forward_request + strings[connection_idx + 10:]
-			else :
-				forward_request = forward_request + strings[connection_idx + 10: second_connection_idx] + 'close' + strings[connection_idx + 10:]
-			# split the payload to HTTP initial line and header
-			request, headers = strings.split('\r\n', 1)
-			# modify the HTTP version
-			forward_port = 80
-			forward_host = ""
-			list_headers = headers.split('\r\n')
-			for header in list_headers:
-				if header.lower().startswith('host:'):
-					host_port = header.split(':')
-					if(len(host_port) == 3):
-						forward_port = int(host_port[2])
-					forward_host = host_port[1]
+                # if this line has the keep-alive keyword
+                elif "keep-alive" in currLine.lower():
+                    lineSplit = re.split(' :|: |:', currLine)
+                    currLine = lineSplit[0] + ": " + "close\r\n"
 
-			request_word = request.split(' ')[0]
-		except Exception:
-			return
-		else:
-			print datetime.datetime.now().strftime("%d %b %X"), '- >>>', request_word, forward_host, ':', forward_port
-			if request_word == 'CONNECT':
-				self.connect_to_server(forward_port, forward_host, data)
-			else:
-				self.nonconnect_to_server(forward_port, forward_host, forward_request)
+                # Don't add request to header
+                if currLine is not request:
+                    header += currLine
+                currLine = ""
+        
+        # Uncomment to print header, server name and server port
+        #print(header)
+        #print(serverName, serverPort)
 
+        # Establish connection between proxy and server
+        serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	def connect_to_server(self, forward_port, forward_host, data):
-		sock = socket.socket(socket.AF_INET, # Internet
-		                    socket.SOCK_STREAM) # TCP
-		
-		forward_ip = socket.gethostbyname(forward_host.strip())
-		return_message = ""
-		try:
-			sock.connect((forward_host.strip(), forward_port))
-			return_message = "HTTP/1.0 200 OK\r\n\r\n"
-			self.client_conn.send(str.encode(return_message))
-		except Exception:
-			return_message = "HTTP/1.0 502 Bad Gateway\r\n\r\n"
-			self.client_conn.send(str.encode(return_message))
-			sock.close()
-			return
+        try:
+            serverSock.connect((serverName, serverPort))
+        except Exception:
+            print("error connecting from proxy to server, sending 502 Bad Gateway")
+            self.bSock.send("HTTP/1.0 502 Bad Gateway\r\n\r\n".encode())
+            serverSock.close()
+            self.bSock.close()
+            return
+        
+        isConnect = "CONNECT" in request
 
-		self.client_conn.settimeout(0.5)
-		sock.settimeout(0.5)
-		max_bytes = 8192
-		from_client = True
+        if isConnect:
+            # Send back 200 OK for successful CONNECT
+            self.bSock.send("HTTP/1.0 200 OK\r\n\r\n".encode())
+        
+        if not isConnect:
+            serverSock.send(request.encode())
+            serverSock.send(header.encode())
+            while True:
+                try:
+                    data = self.bSock.recv(256)
+                except socket.error:
+                    break
+                if len(data) == 0:
+                    break
+                serverSock.send(data)
+            
+            # Get server's header
+            header = ""
+            currLine = ""
+            while True:
+                if header[-4:] == "\r\n\r\n" or header[-3:-1] == "\n\r\n":
+                    # We got the whole header
+                    break
+                currLine += serverSock.recv(1).decode()
+                if currLine[-1:] == "\n":
+                    if "keep-alive" in currLine.lower():
+                        lineSplit = re.split(' :|: |:', currLine)
+                        currLine = lineSplit[0] + ": " + "close\r\n"
+                    header += currLine
+                    currLine = ""
+            self.bSock.send(header.encode())
 
+            # send rest of server's data
+            while True:
+                data = serverSock.recv(256)
+                if len(data) == 0:
+                    break
+                self.bSock.send(data)
+            serverSock.close()
+            self.bSock.close()
+        else:
+            self.bSock.settimeout(30)
+            serverSock.settimeout(30)
+            sendThread = connectThread(self.bSock, serverSock)
+            recvThread = connectThread(serverSock, self.bSock)
+            sendThread.start()
+            recvThread.start()
 
-		while 1:
-			if from_client:
-				try:
-					client_result = self.client_conn.recv(max_bytes)
-					sock.send(client_result)
-				except socket.timeout:
-					from_client = not from_client
-					continue
-				except Exception:
-					continue
-				else:
-					if len(client_result) == 0:
-						self.client_conn.close()
-						break
-			else:
-				try:
-					server_result = sock.recv(max_bytes)
-					self.client_conn.send(server_result)
-				except socket.timeout:
-					from_client = not from_client
-					continue
-				except Exception:
-					continue
-				else:
-					if len(server_result) == 0:
-						sock.close()
-						break
-		sock.close()
+class connectThread(threading.Thread):
+    def __init__(self, srcSock, dstSock):
+        threading.Thread.__init__(self)
+        self.src = srcSock
+        self.dst = dstSock
+        
+    def run(self):
+        sent = False
+        while True:
+            try:
+                data = self.src.recv(256)
+            except socket.timeout:
+                continue
+            except (OSError, ConnectionAbortedError):
+                break
+            if data == 0:
+                break
+            sent = True
+            try:
+                self.dst.send(data)
+            except (OSError, ConnectionAbortedError):
+                break
 
-
-	def nonconnect_to_server(self, forward_port, forward_host, forward_request):
-		sock = socket.socket(socket.AF_INET, # Internet
-		                    socket.SOCK_STREAM) # TCP
-		
-		forward_ip = socket.gethostbyname(forward_host.strip())
-		sock.connect((forward_host.strip(), forward_port))
-		sock.send(forward_request)
-
-		max_bytes = 1024
-		result = sock.recv(max_bytes)
-		strings = bytes(result)
-
-		content_len_index = strings.find("Content-Length")
-		content_length = 0
-		if (content_len_index != -1):
-			content_len_end_index = strings.find('\r\n', content_len_index)
-			content_len_start_index = strings.find(':', content_len_index)
-			content_length = int(strings[content_len_start_index + 1 : content_len_end_index])
-
-		while (content_length > 0):
-			self.client_conn.send(result)
-			result = sock.recv(max_bytes)
-			content_length -= max_bytes
-
-		if (not strings == None):
-			self.client_conn.send(result)
-
-		sock.close()
-
+def Main():
+    port = int(argv[1])
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((LOCAL_HOST, port))
+    try:
+        while True:
+            sock.listen()
+            browserSock, browserAddr = sock.accept()
+            browserSock.setblocking(0)
+            newThread = BrowserThread(browserSock, browserAddr)
+            newThread.daemon = True  # Allows us to kill all threads when main thread exits
+            newThread.start()
+    except (KeyboardInterrupt, SystemExit):  # Catch ctrl + c
+        sock.close()
+        exit()
+        
 
 
 if __name__ == '__main__':
-	tcp_port = sys.argv[1]
-	
-	server_ip = socket.gethostbyname("localhost")
-	proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	proxy_socket.bind((server_ip, int(tcp_port)))
-	proxy_socket.listen(200)
-	print datetime.datetime.now().strftime("%d %b %x"), '- Proxy listening on 0.0.0.0:', sys.argv[1]
-	while 1:
-		try:
-			client_conn, client_addr = proxy_socket.accept()
-			handler = Client_handler(proxy_socket, client_conn)
-			handler.daemon = True
-			handler.start()
-		except KeyboardInterrupt, SystemExit:
-			proxy_socket.close()
-			sys.exit()
-	proxy_socket.close()
+    Main()
