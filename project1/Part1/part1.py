1306579cad48e340d741fa0b46193651b68d1cfc
xuqian511 <xu.qi.an@hotmail.com>
2021-04-18 00:11:14 -0700

    Update part1.py

--- project1/Part1/part1.py
+++ project1/Part1/part1.py
@@ -1,76 +1,125 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 15 12:42:25 2020
-
-@author: fghjk
-"""
-
+import time
 import socket
+import random
 from struct import pack, unpack
 
-UDP_PORT=12235 
-presecret=0 
-cur_step=1 
-sid=128 
-chunk_size=1024 
-byte_alignment=4 
-timeout=1 
-Domain='attu2.cs.washington.edu' 
-print("Beginning Stage A")
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-message='hello world'+'\0'
-header=pack('>iihh',len(message),presecret,cur_step,sid)+message.encode('utf-8')
-sock.sendto(header,(Domain,UDP_PORT))
-result = sock.recv(chunk_size)
-#print(unpack('>iihhiiii', result))
-payload_len, psecret, step, sid, num, length, UDP_PORT, secretA = unpack('>iihhiiii', result)
-print('payload_len:',payload_len,'presecret',psecret,'step',step,'student id#last 3 digits',sid, 'num',\
-      num,'len', length,'udp_port',UDP_PORT,'secretA',secretA)
-print("Beginning Stage B")
-i=0
-while i<num:
-    header = pack('>iihhi', length + byte_alignment, secretA, cur_step, sid, i)
-    if length % byte_alignment !=0:
-        payload=bytearray(length+(byte_alignment-length%byte_alignment))
-    else:
-        payload=bytearray(length)
-    message=header+payload
-    sock.sendto(message, (Domain, UDP_PORT))
-    try:
-        sock.settimeout(timeout)
-        ans = sock.recv(chunk_size)
-    except socket.timeout:
-        i -= 1
-    i += 1
-result = sock.recv(chunk_size)
-#print(unpack('>iihhii', result))
-payload_len, psecret, step, sid, TCP_port, secretB = unpack('>iihhii', result)
-print('payload_len:',payload_len,'presecret',psecret,'step',step,'student id#last 3 digits',sid,\
-      'tcp_port',TCP_port,'secretB',secretB)
-print("Beginning Stage C")
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((Domain,TCP_port))
-sock.settimeout(timeout)
-ans = sock.recv(chunk_size)
-#print(unpack('>iihhiiic', ans[0:25]))
-payload_len, psecret, step, sid, num2, len2, secretC, c= unpack('>iihhiiic', ans[0:25])
-print('payload_len:',payload_len,'presecret',psecret,'step',step,'student id#last 3 digits',sid,\
-      'num2',num2,'len2',len2,'secretC',secretC)
-print("Beginning Stage D")
-i=0
-header = pack('>iihh', len2, secretC, cur_step, sid)
-payload=c
-if len2 % byte_alignment==0:
-    for i in range(1,len2):
-        payload+=c
-else:
-    for i in range(1,len2+byte_alignment-len2%byte_alignment):
-        payload+=c
-message=header+payload
-for i in range(num2):
-    sock.send(message)
-sock.settimeout(timeout)
-ans = sock.recv(chunk_size)
-payload_len, psecret, step, sid, secretD = unpack('>iihhi', ans)
-print('payload_len',payload_len,'presecret',psecret,'step',step,'student id#last3 digits',sid,'secretD',secretD)
-#print(unpack('>iihhi', ans))
+Std_Num = 199
+SERVER_NAME = 'attu2.cs.washington.edu'
+PORT = 12235
+BUFFER_SIZE = 1024
+TIMEOUT = 0.5
+STEP_NUM = 1
+
+def get_header(payload_len:int, psecret:int, step:int ,Std_Num:int) -> bytes:
+    # big-endian
+    header = payload_len.to_bytes(4, byteorder = 'big')
+    header += psecret.to_bytes(4, byteorder = 'big')
+    header += step.to_bytes(2, byteorder = 'big')
+    # header += std_id.to_bytes(2, byteorder = 'big')
+    # header += text.to_bytes(2, byteorder = 'big')
+    header += Std_Num.to_bytes(2, byteorder = 'big')
+    return header
+
+
+
+def Stage_a():
+    # Internet & UDP
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+    # Creating a Socket
+    with sock as s_a:
+        # no padding,'hello world' + '\0'= 12 bytes
+        text = get_header(12, 0, STEP_NUM, Std_Num)
+        message = 'hello world' + '\0'
+        text += str.encode(message, encoding = 'utf-8')
+        s_a.sendto(text,(SERVER_NAME, PORT))
+        # Reference:convert the address recieved from a recvfrom() function to str
+        # https://stackoverflow.com/questions/25524431/python-how-to-convert-the-address-recieved-from-a-recvfrom-function-to-a-stri
+        pbytes, addr = s_a.recvfrom(BUFFER_SIZE)
+
+        global num
+        global packet_len
+        global udp_port 
+        global secretA
+        
+        num = int.from_bytes(pbytes[12:16], byteorder = 'big')
+        packet_len = int.from_bytes(pbytes[16:20], byteorder = 'big')
+        udp_port = int.from_bytes(pbytes[20:24], byteorder = 'big')
+        secretA = int.from_bytes(pbytes[24:], byteorder = 'big')
+
+        print(' Beginning Stage A')
+        print(' Student ID#last 3 digits:',Std_Num,'\n','num:',num,'\n',\
+        'packet_len:', packet_len,'\n','udp_port:',udp_port,'\n',\
+        'secretA:',secretA,'\n')
+    s_a.close()
+
+
+def Stage_b():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+    stage_b_final = None
+    print(' Beginning Stage B')
+    with sock as s_b:
+        packet_id = 0
+        for packet_id in range(num):
+            text = get_header(packet_len + 4, secretA, 1, Std_Num)
+            text += packet_id.to_bytes(4, byteorder = 'big')
+            text += packet_id.to_bytes(packet_len, byteorder = 'big')
+            # i = 0
+            # while i<num:
+            # header = pack( packet_len + 4, secretA, 1, 199, i)
+            # if packet_len % 4 != 0:
+            # payload=bytearray(packet_len+(4-packet_len%4))
+            # else:
+            # payload=bytearray(packet_len)
+            # message=header+payload
+            # sock.sendto(message, (SERVER_NAME, udp_port))
+            # try:
+            # sock.settimeout(timeout)
+            # ans = sock.recv(chunk_size)
+            # except socket.timeout:
+            # i -= 1
+            # i += 1
+            # result = s_b.recvfrom(BUFFER_SIZE)
+            while (1):
+                s_b.settimeout(TIMEOUT)
+                try:
+                    data, addr = s_b.recvfrom(BUFFER_SIZE)
+                   
+        # global tcp_port
+        # global secretB
+        # tcp_port = int.from_bytes(stage_b_final[12:16], byteorder = 'big')
+        # secretB = int.from_bytes(stage_b_final[16:20], byteorder = 'big')
+        # print(' packet_id:', packet_id,'\n','tcp_port:', tcp_port,'\n',\
+        # 'secretB:', secretB, '\n')
+    
+    s_b.close()
+
+def Stage_c():
+    global sock
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((SERVER_NAME, tcp_port))
+    pbytes, addr = sock.recvfrom(BUFFER_SIZE)
+
+    global c
+    global num2
+    global secretC    
+    global packet_len2
+  
+    num2 = int.from_bytes(pbytes[12:16], byteorder='big')
+    packet_len2 = int.from_bytes(pbytes[16:20], byteorder='big')
+    secretC = int.from_bytes(pbytes[20:24], byteorder='big')
+    c = bytes.decode(pbytes[24:25], encoding='utf-8') 
+    print(' Beginning Stage C')
+    print(' num2:', num2,'\n','packet_len2:', packet_len2,'\n',\
+    'secretC:',secretC,'\n')
+
+
+
+
+
+if __name__ == '__main__':
+    
+    Stage_a()
+    Stage_b()
+    Stage_c()
+  
+    socket.socket(socket.AF_INET, socket.SOCK_STREAM).close()
